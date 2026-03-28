ec814d9845dbbf89f7249238bb0ae3821c57fb34
MahiAmul <amithbn90@gmail.com>
2026-03-28 19:13:12 +0530

    second commit

--- src/main/java/com/azure/coderest/CoderestApplication.java
+++ src/main/java/com/azure/coderest/CoderestApplication.java
@@ -2,9 +2,17 @@ package com.azure.coderest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CoderestApplication {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoderestApplication.class, args);
