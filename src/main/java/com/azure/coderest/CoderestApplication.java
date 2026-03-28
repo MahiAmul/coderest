1ccc436fc6d96225104cc2207a80ad76d5a31d06
MahiAmul <amithbn90@gmail.com>
2026-03-28 19:09:20 +0530

    first commit

---
+++ src/main/java/com/azure/coderest/CoderestApplication.java
@@ -0,0 +1,13 @@
+package com.azure.coderest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderestApplication.class, args);
+	}
+
+}
